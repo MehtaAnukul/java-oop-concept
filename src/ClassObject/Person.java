eb93255210b1282e610344f01201b1ea435767ad
anukul mehta <mehtaanukul@gmail.com>
2018-03-10 19:25:43 +0530

    first commit

---
+++ src/ClassObject/Person.java
@@ -0,0 +1,17 @@
+package ClassObject;
+
+public class Person {
+
+    public void toWalk() {
+        System.out.println("Walking Start");
+    }
+
+    public void toEarn() {
+        System.out.println("Earning Start");
+    }
+
+
+    public void toSleep(int hour) {
+        System.out.println("Sleep Time " + hour);
+    }
+}
---
+++ src/ClassObject/RunPerson.java
@@ -0,0 +1,19 @@
+package ClassObject;
+
+/**
+ * Created by Anukul-PC on 10-03-2018.
+ */
+public class RunPerson {
+
+    public static void main(String[] args) {
+        Person anukulPerson = new Person();
+        anukulPerson.toWalk();
+        anukulPerson.toEarn();
+        anukulPerson.toSleep(2000);
+
+        Person mimohPerson = new Person();
+        mimohPerson.toWalk();
+        mimohPerson.toEarn();
+        mimohPerson.toSleep(4000);
+    }
+}
